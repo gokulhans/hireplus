90d4cbc6ea5e54a1fb2d33beb29837fa96511d06
gokul <gokulhansv@gmail.com>
2023-10-08 10:25:38 +0530

    new

--- lib/main.dart
+++ lib/main.dart
@@ -3,12 +3,15 @@ import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
 import 'package:get/get.dart';
 import 'package:hireplus/pages/splashscreen/splashscreen.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
+  await FirebaseAuth.instance.useAuthEmulator('localhost', 9099);
   runApp(const MyApp());
 }
 
